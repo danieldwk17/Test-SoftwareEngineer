ceff31f51ba833d9272524229bfe77882ff56b61
nazwafridaa <fridanazwa55@gamil.com>
2025-06-15 11:39:33 +0700

    first commit

---
+++ Project_Test/About.aspx
@@ -0,0 +1,124 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="Project_Test.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <style>
+        table.repeater-table {
+            width: 100%;
+            border-collapse: collapse;
+            font-family: Arial, sans-serif;
+        }
+
+            table.repeater-table th,
+            table.repeater-table td {
+                border: 1px solid #ccc;
+                padding: 8px 10px;
+                text-align: center;
+            }
+
+            table.repeater-table th {
+                background-color: #007BFF;
+                color: white;
+            }
+
+            table.repeater-table tr:nth-child(even) {
+                background-color: #f9f9f9;
+            }
+
+            table.repeater-table tr:hover {
+                background-color: #f1f1f1;
+            }
+
+        .action-link {
+            color: #007BFF;
+            text-decoration: none;
+            margin: 0 5px;
+        }
+
+            .action-link:hover {
+                text-decoration: underline;
+            }
+    </style>
+
+    <div id="viewRiwayatProduksi" runat="server">
+        <h3>Riwayat Perataan Produksi</h3>
+        <asp:Button ID="btnAddForm" runat="server" Text="Add Form" OnClick="btnAddForm_Click" />
+        <br />
+        <br />
+        <asp:Repeater ID="rptRiwayat" runat="server" OnItemCommand="rptRiwayat_ItemCommand">
+            <HeaderTemplate>
+                <table border="1" cellpadding="5" cellspacing="0" class="repeater-table">
+                    <tr style="background-color: #f0f0f0;">
+                        <th>Tanggal</th>
+                        <th>Senin</th>
+                        <th>Selasa</th>
+                        <th>Rabu</th>
+                        <th>Kamis</th>
+                        <th>Jumat</th>
+                        <th>Sabtu</th>
+                        <th>Minggu</th>
+                        <th>Senin (Fix)</th>
+                        <th>Selasa (Fix)</th>
+                        <th>Rabu (Fix)</th>
+                        <th>Kamis (Fix)</th>
+                        <th>Jumat (Fix)</th>
+                        <th>Sabtu (Fix)</th>
+                        <th>Minggu (Fix)</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <tr>
+                    <td><%# Eval("dCreatedAt", "{0:dd-MM-yyyy HH:mm}") %></td>
+                    <td><%# Eval("iSenin") %></td>
+                    <td><%# Eval("iSelasa") %></td>
+                    <td><%# Eval("iRabu") %></td>
+                    <td><%# Eval("iKamis") %></td>
+                    <td><%# Eval("iJumat") %></td>
+                    <td><%# Eval("iSabtu") %></td>
+                    <td><%# Eval("iMinggu") %></td>
+                    <td><%# Eval("iSeninFix") %></td>
+                    <td><%# Eval("iSelasaFix") %></td>
+                    <td><%# Eval("iRabuFix") %></td>
+                    <td><%# Eval("iKamisFix") %></td>
+                    <td><%# Eval("iJumatFix") %></td>
+                    <td><%# Eval("iSabtuFix") %></td>
+                    <td><%# Eval("iMingguFix") %></td>
+                    <td style='<%# Convert.ToInt32(Eval("iIsActive")) == 1 ? "color:green;": "color:red;" %>'>
+                        <%# Convert.ToInt32(Eval("iIsActive")) == 1 ? "Aktif" : "Tidak Aktif" %>
+                    </td>
+                    <td>                        
+                        <asp:LinkButton ID="lnkDelete" runat="server" CommandName="hapus" CommandArgument='<%# Eval("iId") %>' OnClientClick="return confirm('Yakin ingin menghapus data ini?');">Delete</asp:LinkButton>
+                    </td>
+
+                </tr>
+            </ItemTemplate>
+            <FooterTemplate>
+                </table>
+            </FooterTemplate>
+        </asp:Repeater>
+    </div>
+    <div id="formAdd" runat="server" visible="false">
+        <div>
+            <h2>Input Produksi Mobil (7 Hari Kerja)</h2>
+            <asp:Label ID="lblTanggal" runat="server" Text="Tanggal Rencana Produksi:"></asp:Label><br />
+            <asp:TextBox ID="txtTanggalProduksi" runat="server" TextMode="Date" CssClass="form-control"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtSenin" runat="server" placeholder="Senin"></asp:TextBox>
+            <asp:TextBox ID="txtSelasa" runat="server" placeholder="Selasa"></asp:TextBox>
+            <asp:TextBox ID="txtRabu" runat="server" placeholder="Rabu"></asp:TextBox>
+            <asp:TextBox ID="txtKamis" runat="server" placeholder="Kamis"></asp:TextBox>
+            <asp:TextBox ID="txtJumat" runat="server" placeholder="Jumat"></asp:TextBox>
+            <asp:TextBox ID="txtSabtu" runat="server" placeholder="Sabtu"></asp:TextBox>
+            <asp:TextBox ID="txtMinggu" runat="server" placeholder="Minggu"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnProses" runat="server" Text="Proses" OnClick="btnProses_Click" />
+            <br />
+            <br />
+            <asp:Literal ID="ltHasil" runat="server"></asp:Literal>
+            <br />
+            <asp:Button ID="btnSimpan" runat="server" OnClick="btnSimpan_Click" Text="Simpan" Visible="false" />
+        </div>
+    </div>
+</asp:Content>
---
+++ Project_Test/About.aspx.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_Test
+{
+    public partial class About : Page
+    {
+        TeknikalTestEntities entities = new TeknikalTestEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();                
+            }
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                var data = entities.TBL_PRODUKSI
+                     .OrderByDescending(x => x.dCreatedAt)
+                     .ToList();
+
+                rptRiwayat.DataSource = data;
+                rptRiwayat.DataBind();
+
+                viewRiwayatProduksi.Visible = true;
+                formAdd.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        protected void btnProses_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] input = new int[7];
+                try
+                {
+                    input[0] = int.Parse(txtSenin.Text);
+                    input[1] = int.Parse(txtSelasa.Text);
+                    input[2] = int.Parse(txtRabu.Text);
+                    input[3] = int.Parse(txtKamis.Text);
+                    input[4] = int.Parse(txtJumat.Text);
+                    input[5] = int.Parse(txtSabtu.Text);
+                    input[6] = int.Parse(txtMinggu.Text);
+                }
+                catch
+                {
+                    ltHasil.Text = "Pastikan semua input adalah angka!";
+                    return;
+                }
+
+                var hariAktif = input
+                    .Select((val, idx) => new { val, idx })
+                    .Where(x => x.val > 0)
+                    .ToList();
+
+                int jumlahHariAktif = hariAktif.Count;
+                int total = hariAktif.Sum(x => x.val);
+                int rata = total / jumlahHariAktif;
+                int sisa = total % jumlahHariAktif;
+
+                int[] hasil = new int[7];
+
+                foreach (var h in hariAktif)
+                {
+                    hasil[h.idx] = rata;
+                }
+
+                var prioritas = hariAktif
+                    .OrderByDescending(x => x.val)
+                    .Select(x => x.idx)
+                    .ToList();
+
+                for (int i = 0; i < sisa; i++)
+                {
+                    hasil[prioritas[i]] += 1;
+                }
+
+                // Ini Output
+                ltHasil.Text = "<b>Rencana Produksi yang Diratakan:</b><br/>" +
+                    $"Senin: {hasil[0]}<br/>" +
+                    $"Selasa: {hasil[1]}<br/>" +
+                    $"Rabu: {hasil[2]}<br/>" +
+                    $"Kamis: {hasil[3]}<br/>" +
+                    $"Jumat: {hasil[4]}<br/>" +
+                    $"Sabtu: {hasil[5]}<br/>" +
+                    $"Minggu: {hasil[6]}";
+
+                ViewState["Input"] = input;
+                ViewState["Hasil"] = hasil;
+
+                btnSimpan.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        private void ClearData()
+        {
+            txtTanggalProduksi.Text = string.Empty;
+            txtSenin.Text = string.Empty;
+            txtSelasa.Text = string.Empty;
+            txtRabu.Text = string.Empty;
+            txtKamis.Text = string.Empty;
+            txtJumat.Text = string.Empty;
+            txtSabtu.Text = string.Empty;
+            txtMinggu.Text = string.Empty;
+            ltHasil.Text = string.Empty;
+            btnSimpan.Visible = false;
+        }
+
+        protected void btnAddForm_Click(object sender, EventArgs e)
+        {
+            viewRiwayatProduksi.Visible = false;
+            formAdd.Visible = true;
+            ClearData();
+        }
+
+        private void SimpanKeDatabase(int[] input, int[] hasil)
+        {
+            var data = new TBL_PRODUKSI()
+            {
+                iSenin = input[0],
+                iSelasa = input[1],
+                iRabu = input[2],
+                iKamis = input[3],
+                iJumat = input[4],
+                iSabtu = input[5],
+                iMinggu = input[6],
+                iSeninFix = hasil[0],
+                iSelasaFix = hasil[1],
+                iRabuFix = hasil[2],
+                iKamisFix = hasil[3],
+                iJumatFix = hasil[4],
+                iSabtuFix = hasil[5],
+                iMingguFix = hasil[6],
+                dCreatedAt = DateTime.Parse(txtTanggalProduksi.Text),
+                iIsActive = 1,
+                dCreatedDate = DateTime.Now
+            };
+            entities.TBL_PRODUKSI.Add(data);
+            entities.SaveChanges();
+        }
+
+        protected void rptRiwayat_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            int id = int.Parse(e.CommandArgument.ToString());
+
+            if (e.CommandName == "detail")
+            {
+                ClearData();                
+            }
+            else if (e.CommandName == "hapus")
+            {
+                var item = entities.TBL_PRODUKSI.Find(id);
+                if (item != null)
+                {
+                    item.iIsActive = 0;
+                    item.dModifiedDate = DateTime.Now;
+                    entities.SaveChanges();
+                    LoadData();
+                }
+            }
+        }
+
+        protected void btnSimpan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ViewState["Input"] != null && ViewState["Hasil"] != null)
+                {
+                    int[] input = (int[])ViewState["Input"];
+                    int[] hasil = (int[])ViewState["Hasil"];
+
+                    SimpanKeDatabase(input, hasil);
+                    LoadData();
+
+                    ScriptManager.RegisterStartupScript(this, GetType(), "sukses",
+                    "Swal.fire({ icon: 'success', title: 'Berhasil', text: 'Data berhasil disimpan!' });", true);
+                }
+                else
+                {
+                    ltHasil.Text = "Silakan proses data terlebih dahulu sebelum menyimpan.";
+                }
+            }
+            catch(Exception ex)
+            {
+                ex.ToString();
+                ScriptManager.RegisterStartupScript(this, GetType(), "gagal",
+                "Swal.fire({ icon: 'error', title: 'Oops!', text: 'Terjadi kesalahan saat menyimpan!' });", true);
+            }
+        }
+    }
+}
